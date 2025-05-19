656d99a8ebdb4b9188e815b6c6450ed09461fa82
Charlie-033 <ahmedeleus1@gmail.com>
2025-05-19 21:38:45 +0600

    Create Mongoclient with new database and successfully connected to MongoDB

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -5,6 +6,35 @@ const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
+
+// 8cmIeHFeopGT7SVc
+// Plant-Care-Tracker
+
+const uri = "mongodb+srv://Plant-Care-Tracker:8cmIeHFeopGT7SVc@cluster001.bmpze7a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster001";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
